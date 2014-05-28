76d4c16808356837ffa223bf881252b04e271865
Lucius Choi <rorlab@gmail.com>
2014-05-28 17:30:06 +0900

    Post 리소스 생성후 마이그레이션 함

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   root 'welcome#index'
   devise_for :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528023115) do
+ActiveRecord::Schema.define(version: 20140528082305) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
