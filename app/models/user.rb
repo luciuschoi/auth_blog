aed34d335f57a5a2213a083d52b9cfb45e02c1e3
Lucius Choi <rorlab@gmail.com>
2014-06-01 15:58:21 +0900

    omniauth-facebook 설정

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+
   has_many :posts, dependent: :destroy
 
   after_create :set_default_role, if: Proc.new { User.count > 1 }
