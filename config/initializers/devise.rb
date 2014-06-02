6ffbfc8b0145a78070ba76c1623fa3c632a9701c
Lucius Choi <rorlab@gmail.com>
2014-06-02 10:30:18 +0900

    devise.rb 업데이트

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,7 +231,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"],
-      {:scope => 'email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+      {:scope => 'email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/SecureTrust_CA.pem'}}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
